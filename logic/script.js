40ec813b4f300b071122920daf211ae6b3a9a705
AhmedMekkawy27 <mekkawy39@gmail.com>
2024-09-13 11:28:49 +0300

    notification feature

--- logic/script.js
+++ logic/script.js
@@ -2,39 +2,45 @@ const todoList = document.getElementById('todo-list');
 let tasks;
 let updateStatus = false;
 const todoInput = document.getElementById('todo-input');
+
 if(localStorage.getItem('tasks')){
+    // Get tasks from local storage and display them on the page
     tasks = JSON.parse(localStorage.getItem('tasks'));
     for(let i = 0; i < JSON.parse(localStorage.getItem('tasks')).length; i++){
         showtasks(JSON.parse(localStorage.getItem('tasks'))[i].text)
+
+        // Check if there are any completed tasks in the local storage
         if(JSON.parse(localStorage.getItem('tasks'))[i].completed === true){
             document.querySelectorAll('li')[i].classList.toggle('completed');
         }
     }
 }else{
+    // Initialize an empty array if no tasks are found in local storage
     tasks = [];
 }
 
-
-
+// Read Operation
 document.getElementById('add-btn').addEventListener('click', function() {
     let task = {
         text: todoInput.value.trim(),
         completed: false
     };
     if (task.text !== "" && updateStatus === false) {
-        
+        // Update tasks array in the local storage if it exists
         tasks.push(task)
         localStorage.setItem('tasks', JSON.stringify(tasks));
         
-        // Append task to list
+        // Show new task in ui
         showtasks(task.text)
 
+        // Notify user that task has been added
+        notify('Added')
         // Clear input field
         todoInput.value = '';
     }
 });
 
-// Read Operation
+// Read Function
 function showtasks(tasks) { 
         const li = document.createElement('li');
         li.classList.add('todo-item');
@@ -52,9 +58,13 @@ function showtasks(tasks) {
  document.onclick = function(e){
     // Remove Operation
     if(e.target.classList.contains('remove-btn')){
+        // Removing task from ui and upadating local storage
         e.target.parentElement.parentElement.remove();
         tasks = tasks.filter(task => task.text!== e.target.parentElement.parentElement.querySelector('span').innerText);
         localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        // Notify user that task has been deleted
+        notify('deleted')
     } 
 
     // Update Operation
@@ -75,12 +85,15 @@ function showtasks(tasks) {
                 todoInput.value = e.target.parentElement.previousElementSibling.innerHTML = todoInput.value.trim();
                 localStorage.setItem('tasks', JSON.stringify(tasks));
                 todoInput.value = ''
-                document.getElementById('add-btn').textContent = 'Add task';
+                document.getElementById('add-btn').textContent = 'Add Task';
                 todoInput.placeholder = 'Add a new task';
                 updateStatus = false;
+                notify('Updated')
             }
         })
     }
+    
+    // Complete/Incomplete Operation
     else if(e.target.classList.contains('todo-item')){
         for(let i = 0; i < tasks.length; i++){
             if(tasks[i].text === e.target.parentElement.querySelector('span').innerText){
@@ -91,4 +104,13 @@ function showtasks(tasks) {
         }
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
+ }
+
+ // show notification message
+ function notify(message){
+    document.querySelector('.message').textContent = `You ${message} a task`
+    document.querySelector('.message').style.display = 'block';
+    setTimeout(() => {
+        document.querySelector('.message').style.display = 'none';
+    }, 2000);
  }
